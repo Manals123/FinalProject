90314b97903ecc39fa73d05d95043efd51722e6e
Manals123 <110411924+Manals123@users.noreply.github.com>
2022-11-24 20:18:40 +0200

    Update FinalProject.java
    fix

--- ManalProject/src/test/java/FinalProject.java
+++ ManalProject/src/test/java/FinalProject.java
@@ -3,20 +3,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import  org.openqa.selenium.WebElement;
+import java.util.List;
+
 public class FinalProject {
-    //   public WebDriver driver = null;
+    public WebDriver driver = null;
+
 
     @BeforeEach
     void intiallaizedSelenium() {
         System.setProperty("web-driver.chrome.driver", "res/chromedriver");
-        //WebDriver driver = new ChromeDriver();
+
     }
 
+    //question no 1
     @Test
 //Test for registration
-    void harsh() throws InterruptedException {
+    void finalProject() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("http://tutorialsninja.com/demo/index.php?route=account/register");
         Thread.sleep(500);
@@ -34,9 +38,9 @@ public class FinalProject {
 
         eMail.sendKeys("Manals277@gmail.com");
         Thread.sleep(500);
-        var telUser = driver.findElement(By.name("telephone"));
+        var telephone = driver.findElement(By.name("telephone"));
         Thread.sleep(500);
-        telUser.sendKeys("0509057710");
+        telephone.sendKeys("0509057710");
 
         var passWord = driver.findElement(By.id("input-password"));
         Thread.sleep(500);
@@ -48,6 +52,7 @@ public class FinalProject {
         confirmPass.sendKeys("Manal123");
         Thread.sleep(500);
 
+
         var subScribe = driver.findElement(By.xpath("/html/body/div[2]/div/div/form/fieldset[3]/div/label"));
         Thread.sleep(500);
         var noButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/form/fieldset[3]/div/div/label[2]/input"));
@@ -55,25 +60,24 @@ public class FinalProject {
         noButton.click();
         Thread.sleep(500);
 
-        var privacyButton = driver.findElement(By.name("agree"));
+        var privecyButton = driver.findElement(By.name("agree"));
         Thread.sleep(500);
-        privacyButton.click();
+        privecyButton.click();
         Thread.sleep(500);
 
         var continueButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/form/div/div/input[2]"));
         Thread.sleep(500);
         continueButton.click();
         Thread.sleep(500);
+
         Assertions.assertEquals("http://tutorialsninja.com/demo/index.php?route=account/success", driver.getCurrentUrl());
     }
 
 
-
-
-        //question no 2
+    //question no 2
     //log in test
     @Test
-    void logIn () throws InterruptedException {
+    void logIn() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("http://tutorialsninja.com/demo/index.php?route=account/login");
         Thread.sleep(500);
@@ -93,34 +97,42 @@ public class FinalProject {
         logIn.click();
         Thread.sleep(500);
 
-        Assertions.assertEquals("http://tutorialsninja.com/demo/index.php?route=account/account",driver.getCurrentUrl());
+        Assertions.assertEquals("http://tutorialsninja.com/demo/index.php?route=account/account", driver.getCurrentUrl());
 
     }
 
-
     //question no 3
-//Add to cart test
+    //Add to cart test
     @Test
-    void addToCart() throws InterruptedException {
+    void addToCartTest() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("http://tutorialsninja.com/demo/index.php?route=product/product&product_id=43");
+        driver.get("http://tutorialsninja.com/demo/index.php?route=product/product&product_id=43");//Connection with the site
         Thread.sleep(500);
-        var addButton = driver.findElement(By.id("button-cart"));
+
+        var addButton = driver.findElement(By.id("button-cart"));//Add the 1st product to the cart
         addButton.click();
         Thread.sleep(500);
+
         driver.get("http://tutorialsninja.com/demo/index.php?route=product/product&product_id=33");
         Thread.sleep(500);
-        var addButton1 = driver.findElement(By.id("button-cart"));
+
+        var addButton1 = driver.findElement(By.id("button-cart"));//Add the 2nd product to the cart
         addButton1.click();
         Thread.sleep(500);
 
-
-        // check the total amount //problem with var
-        WebElement total = driver.findElement(By.xpath("/html/body/header/div/div/div[3]/div/ul/li[2]/div/table/tbody/tr[4]/td[2]"));
+        // check the total amount
+        var cartTotalButton = driver.findElement(By.id("cart-total"));
+        cartTotalButton.click();
         Thread.sleep(500);
-        Assertions.assertEquals(total,"$844');
+
+        var totalAmount = driver.findElement(By.xpath("//*[@id=\"cart\"]/ul/li[2]/div/table/tbody/tr[4]/td[2]")).getText();
+        //Getting out the amount as text from Webelement "total"
+        Thread.sleep(500);
+        Assertions.assertEquals("$844.00", totalAmount);//Comepration between the expected amount to the actual amount
+
 
     }
+
 
     //question no 4
 //Opinion about the product
@@ -130,16 +142,58 @@ public class FinalProject {
         WebDriver driver = new ChromeDriver();
         driver.get("http://tutorialsninja.com/demo/index.php?route=product/product&product_id=43");
         var reviewsButton = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[1]/ul[2]/li[3]/a"));
-         reviewsButton.click();
-          Thread.sleep(500);
+        reviewsButton.click();
+        Thread.sleep(500);
 
         var YourName = driver.findElement(By.id("input-name"));
         YourName.sendKeys("Zoya");
         Thread.sleep(500);
-        var YourReview  = driver.findElement(By.id("input-review"));
-        String opnion = " The MacBook site is an excellent site and you can see that the site is clear, convenient and accessible for the customer.";
+
+        List<WebElement> listOfElements = driver.findElements(By.name("rating"));//Choosing the 3rd option between five
+        listOfElements.get(2).click();
+        Thread.sleep(500);
+
+        var continueButton = driver.findElement(By.id("button-review"));
+        continueButton.click();
+
+
+        var YourReview = driver.findElement(By.id("input-review"));
+        String opnion = " The MacBook site is an excellent site and you can see that the site is clear, convenient and accessible for the customer excellent.";
         YourReview.sendKeys(opnion);
+
+        var approvelMesege = driver.findElement(By.xpath("//*[@id=\"form-review\"]/div[2]"));
+        Assertions.assertNotEquals(null, approvelMesege);//Checking the approvel message has recived
+
+        }
     }
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
